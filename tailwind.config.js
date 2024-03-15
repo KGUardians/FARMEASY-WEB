32b147bef5ac3685d10af5e4b35e6cb7d6d2f1c3
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-03-15 21:38:52 +0900

    *init project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import tailwindScrollbarHide from 'tailwind-scrollbar-hide';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindScrollbarHide],
+};
