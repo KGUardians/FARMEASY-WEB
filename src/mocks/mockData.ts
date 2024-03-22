b468501d0ae8050d35c2733cd60bbde932005d64
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-03-22 18:27:10 +0900

    design: MainPage

---
+++ src/mocks/mockData.ts
@@ -0,0 +1,7 @@
+export const communityData = [
+  { id: 1, text: '딸기 수익성 관련 질문', view: 12, like: 4 },
+  { id: 2, text: '딸기 키우기가 너무 힘들어요..', view: 16, like: 7 },
+  { id: 3, text: '요즘 장마철이라 영 별로네요', view: 11, like: 14 },
+  { id: 4, text: '조언 부탁드립니다', view: 8, like: 3 },
+  { id: 5, text: '잡초 제거 방법 질문', view: 136, like: 67 },
+];
--- src/pages/Mainpage/mainpage.tsx
+++ src/pages/Mainpage/mainpage.tsx
@@ -1,5 +1,20 @@
+import { CheckPlantCard } from './CheckPlantCard';
+import { CommunityCard } from './CommunityCard';
+import { MarketCard } from './MarketCard';
+import { RegisterCard } from './RegisterCard';
+
 const MainPage = () => {
-  return <div>mainpage</div>
-}
+  return (
+    <section className="px-6 grid gap-5 pt-5 pb-5">
+      <CheckPlantCard />
+
+      <MarketCard />
+
+      <RegisterCard />
+
+      <CommunityCard />
+    </section>
+  );
+};
 
 export default MainPage;
