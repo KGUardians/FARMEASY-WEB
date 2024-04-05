f01f394dc6b86313ec9d114b7b01238b1ba1b531
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-04-05 09:23:41 +0900

    delete: 기존 마이페이지 삭제

--- src/pages/Mainpage/mainpage.tsx
+++ src/pages/Mainpage/mainpage.tsx
@@ -1,20 +1,5 @@
-import { CheckPlantCard } from './CheckPlantCard';
-import { CommunityCard } from './CommunityCard';
-import { MarketCard } from './MarketCard';
-import { RegisterCard } from './RegisterCard';
-
 const MainPage = () => {
-  return (
-    <section className="px-6 grid gap-5 pt-5 pb-5">
-      <CheckPlantCard />
-
-      <MarketCard />
-
-      <RegisterCard />
-
-      <CommunityCard />
-    </section>
-  );
+  return <section className="px-6 grid gap-5 pt-5 pb-5"></section>;
 };
 
 export default MainPage;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,15 +1,13 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
-import MainPage from '../pages/Mainpage/mainpage';
+import MainPage from '../pages/Mainpage/Mainpage';
 
 const Router = () => (
   <RouterProvider
     router={createBrowserRouter([
       {
         element: <Layout />,
-        children: [
-          { path: '/', element: <MainPage /> },
-        ],
+        children: [{ path: '/', element: <MainPage /> }],
       },
     ])}
   />
