a59a733c13159d94fb41d5ae5a6a7a17f39c641c
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-04-05 09:24:03 +0900

    chore: 커스텀 색상 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 import tailwindScrollbarHide from 'tailwind-scrollbar-hide';
 
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['index.html', './src/**/*.{html,js,jsx,ts,tsx,css}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'fp-green': '#accb7a',
+      },
+    },
   },
   plugins: [tailwindScrollbarHide],
-}
-
+};
