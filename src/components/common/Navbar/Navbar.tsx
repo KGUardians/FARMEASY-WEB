5086934af313d3b73207b5fb955451144d8f891b
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-04-12 09:49:37 +0900

    delete: Mainpage 퍼블리싱 초기화

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -3,11 +3,10 @@ import { IoMenuOutline } from 'react-icons/io5';
 
 export const Navbar = () => {
   return (
-    <div className="sticky z-10 h-14 w-[400px] bg-white top-0 pb-10">
-      <div className="w-full h-1 bg-fp-green"></div>
-      <div className="flex px-2 items-center justify-between">
+    <div className="sticky top-0 z-10 w-full my-3 bg-white h-14">
+      <div className="flex items-center justify-between px-5">
         <img src={logo} alt="farmeasy" className="w-28 h-14" />
-        <IoMenuOutline className="w-14 h-8" />
+        <IoMenuOutline className="w-8 h-8 mt-3" />
       </div>
     </div>
   );
