bb9cdc4987c9308e4b9a31e04c6aab0b77ee6859
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-04-12 09:49:54 +0900

    refactor: Navbar 위치 변경

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -4,8 +4,8 @@ import { Navbar } from '../components/common/Navbar/Navbar';
 const Layout = () => {
   return (
     <main className="bg-slate-200 flex flex-col items-center">
-      <Navbar />
       <section className="min-h-screen bg-white w-[400px] mx-auto overflow-auto scrollbar-hidden">
+        <Navbar />
         <Outlet />
       </section>
     </main>
