138d32ef17d9286ed314cafda0dccc7907033de6
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-04-16 14:09:28 +0900

    refactor: MainPage -> DeterminePage 링크

--- src/pages/Mainpage/MainPage.tsx
+++ src/pages/Mainpage/MainPage.tsx
@@ -1,11 +1,19 @@
 import { MarketSection } from '../../components/Mainpage/MarketSection';
 import { FarmExperienceSection } from '../../components/Mainpage/FarmExperienceSection';
 import { CommunitySection } from '../../components/Mainpage/CommunitySection';
+import determineImage from '../../../public/image/linktodetermine.png';
+import { Link } from 'react-router-dom';
 
 const MainPage = () => {
   return (
     <section>
-      <div className="w-[400px] h-[240px] bg-fp-green"></div>
+      <Link to="/determine">
+        <img
+          src={determineImage}
+          alt="event"
+          className="object-cover w-[400px] h-[240px]"
+        />
+      </Link>
 
       <div className="flex flex-col px-4 mt-4">
         <MarketSection />
