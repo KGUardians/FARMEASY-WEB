22b4e5bc6dc0cfa01be9489cee03f22f49b9d304
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-04-16 21:42:03 +0900

    refactor: Navbar

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import logo from '../../../../public/fullLogo.svg';
 import { IoMenuOutline } from 'react-icons/io5';
+import { useNavigate } from 'react-router-dom';
+import { SideMenu } from './SideMenu';
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <div className="sticky top-0 z-10 w-full my-3 bg-white h-14">
-      <div className="flex items-center justify-between px-5">
-        <img src={logo} alt="farmeasy" className="w-28 h-14" />
-        <IoMenuOutline className="w-8 h-8 mt-3" />
+    <div className="relative top-0 z-10 flex items-center justify-between w-full px-5 mb-3 bg-white select-none pt-7">
+      <img
+        src={logo}
+        alt="farmeasy"
+        className="cursor-pointer w-28 h-14"
+        onClick={() => navigate('/')}
+      />
+      <div className="flex items-center gap-2 mt-3">
+        <button className="text-sm" onClick={() => navigate('/')}>
+          로그인
+        </button>
+        <IoMenuOutline
+          className="w-8 h-8 -mr-1 cursor-pointer"
+          onClick={() => setIsOpen(true)} // 상태를 토글하도록 변경
+        />
       </div>
+
+      <SideMenu open={isOpen} setOpen={setIsOpen} />
     </div>
   );
 };
