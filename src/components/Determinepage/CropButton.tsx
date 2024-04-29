4e9ebf9e49a805a3982eaca253888f362a74f986
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-04-29 20:40:09 +0900

    refactor: 작물 아이콘 변경

--- src/components/Determinepage/CropButton.tsx
+++ src/components/Determinepage/CropButton.tsx
@@ -16,11 +16,11 @@ export const CropButton = ({
   <div key={item.id} className="grid mb-8 place-items-center">
     <button
       onClick={() => handleItemClick(item.id)}
-      className={`flex rounded-full items-center justify-center w-[52px] h-[52px] bg-neutral-100 hover:outline hover:outline-fp-green ${
-        selectedCrop === item.id ? 'outline outline-fp-green' : ''
+      className={`flex rounded-[15px] items-center justify-center w-[52px] h-[52px] bg-neutral-100 hover:outline hover:outline-fp-green ${
+        selectedCrop === item.id ? 'outline outline-2 outline-[#9FC031]' : ''
       }`}
     >
-      <img src={item.icons} alt={item.title} className="w-10" />
+      <img src={item.icons} alt={item.title} />
     </button>
     <p className="my-1 text-xs text-center">{item.title}</p>
   </div>
