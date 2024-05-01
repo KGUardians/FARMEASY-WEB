fa5a113821af61f39e08ad9e4fbe2b79c64eadfe
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-05-01 19:23:57 +0900

    design: 병충해 판별 결과 안내 페이지 퍼블리싱

---
+++ src/pages/Determinepage/ResultPage.tsx
@@ -0,0 +1,50 @@
+import Button from '../../components/common/Button/Button';
+import successIcon from '../../../public/successIcon.svg';
+import failIcon from '../../../public/failIcon.svg';
+import { useNavigate } from 'react-router-dom';
+import useCropStore from '../../store/cropStore';
+
+const ResultPage = () => {
+  const selectedCrop = useCropStore((state) => state.selectedCrop);
+  const navigate = useNavigate();
+
+  const handleSucces = () => {
+    navigate('/');
+  };
+
+  const handleFail = () => {
+    navigate('/');
+  };
+
+  return (
+    <section className="flex flex-col items-center justify-center h-screen">
+      {selectedCrop !== null && selectedCrop >= 1 && selectedCrop <= 5 ? (
+        <>
+          <div className="flex flex-col items-center">
+            <img src={successIcon} alt="success" />
+            <p className="mt-4 text-lg">해충이 발견되지 않았습니다!</p>
+          </div>
+          <div className="absolute top-[640px]">
+            <Button onClick={handleSucces} className="w-[340px]">
+              돌아가기
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex flex-col items-center">
+            <img src={failIcon} alt="fail" />
+            <p className="mt-4 text-lg">ㅇㅇㅇㅇ에 문제가 있는 것 같습니다.</p>
+          </div>
+          <div className="absolute top-[640px]">
+            <Button onClick={handleFail} className="w-[340px]">
+              질문하러 가기
+            </Button>
+          </div>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default ResultPage;
