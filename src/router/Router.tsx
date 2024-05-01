22a2fdbbf1c30162db1d4de1da9ea7d2e1e440b9
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-05-01 19:23:08 +0900

    feat: 라우터 및 레이아웃 수정
    - 일부 페이지 레이아웃 상단바 숨기기 기능 구현

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
 import MainPage from '../pages/Mainpage/MainPage';
 import DeterminePage from '../pages/Determinepage/DeterminePage';
+import ResultPage from '../pages/Determinepage/ResultPage';
 
 const Router = () => (
   <RouterProvider
@@ -11,6 +12,7 @@ const Router = () => (
         children: [
           { path: '/', element: <MainPage /> },
           { path: '/determine', element: <DeterminePage /> },
+          { path: '/determine/result', element: <ResultPage /> },
         ],
       },
     ])}
