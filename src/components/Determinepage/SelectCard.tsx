66b34a3b066ff32a2b795457e5ca7a4223aad423
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-05-02 10:34:08 +0900

    design: DeterminePage 디자인 변경
    - 작물 선택 안내 이미지 및 아이콘 추가

--- src/components/Determinepage/SelectCard.tsx
+++ src/components/Determinepage/SelectCard.tsx
@@ -1,13 +1,24 @@
 import { Link } from 'react-router-dom';
 import { cropData } from '../../mocks/mockData';
 import { CropButton } from './CropButton';
+import useCropStore from '../../store/cropStore';
+import selectCropImage from '../../../public/selectCropFirst.svg';
+import searchIcon from '../../../public/searchIcon.svg';
 
 const SelectCard = () => {
+  const selectedCrop = useCropStore((state) => state.selectedCrop);
+
   return (
     <>
       <Link to="/determine" className="grid justify-items-center">
-        <div className="justify-self-start">
-            <p className="font-semibold">내 작물 병충해 판별</p>
+        <div className="flex justify-self-start">
+          <img src={searchIcon} alt="searchIcon" />
+          <p className="mx-1.5 font-semibold">내 작물 병충해 판별</p>
+          {selectedCrop ? (
+            <></>
+          ) : (
+            <img src={selectCropImage} alt="selectCropFirst" />
+          )}
         </div>
         <div className="grid w-full grid-cols-5 my-4">
           {cropData.map((item) => (
