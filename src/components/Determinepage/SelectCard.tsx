e36cfc1c1b7cf84473fc6a042f3634dc77e87fa1
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-05-03 11:23:45 +0900

    markup: MarketPage

--- src/components/Determinepage/SelectCard.tsx
+++ src/components/Determinepage/SelectCard.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { cropData } from '../../mocks/mockData';
 import { CropButton } from './CropButton';
 import useCropStore from '../../store/cropStore';
@@ -10,22 +9,20 @@ const SelectCard = () => {
 
   return (
     <>
-      <Link to="/determine" className="grid justify-items-center">
-        <div className="flex justify-self-start">
-          <img src={searchIcon} alt="searchIcon" />
-          <p className="mx-1.5 font-semibold">내 작물 병충해 판별</p>
-          {selectedCrop ? (
-            <></>
-          ) : (
-            <img src={selectCropImage} alt="selectCropFirst" />
-          )}
-        </div>
-        <div className="grid w-full grid-cols-5 my-4">
-          {cropData.map((item) => (
-            <CropButton key={item.id} item={item} />
-          ))}
-        </div>
-      </Link>
+      <div className="flex justify-self-start">
+        <img src={searchIcon} alt="searchIcon" />
+        <p className="mx-1.5 font-semibold">내 작물 병충해 판별</p>
+        {selectedCrop ? (
+          <></>
+        ) : (
+          <img src={selectCropImage} alt="selectCropFirst" />
+        )}
+      </div>
+      <div className="grid w-full grid-cols-5 my-4">
+        {cropData.map((item) => (
+          <CropButton key={item.id} item={item} />
+        ))}
+      </div>
     </>
   );
 };
---
+++ src/types/productList.ts
@@ -0,0 +1,11 @@
+export interface ProductInfo {
+  id: number;
+  url: string;
+  like?: boolean;
+  liked: number;
+  type: string;
+  price: number;
+  weight: number;
+  farmName: string;
+  place: string;
+}
