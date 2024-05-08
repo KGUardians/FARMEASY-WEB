4e32008d87a26658ab654b7d6a175c99182147f0
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-05-08 15:50:22 +0900

    feat: Carousel

---
+++ src/components/common/Image/Carousel.tsx
@@ -0,0 +1,26 @@
+import { Carousel } from '@material-tailwind/react';
+
+interface CarouselDefaultProps {
+  url: string[]; 
+}
+
+export function CarouselDefault({ url }: CarouselDefaultProps) {
+
+  return (
+    <Carousel
+      className="h-[244px]"
+      placeholder={undefined}
+      onPointerEnterCapture={undefined}
+      onPointerLeaveCapture={undefined}
+    >
+      {url.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={"carousel image"}
+          className="h-full w-full object-cover"
+        />
+      ))}
+    </Carousel>
+  );
+}
