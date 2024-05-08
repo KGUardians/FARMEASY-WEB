71f3bb8e1689ae0ccaea2f81224f8e11b649120f
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-05-08 20:53:18 +0900

    refactor: 배경색 변경

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -3,7 +3,7 @@ import { Navbar } from '../components/common/Navbar/Navbar';
 
 const Layout = () => {
   return (
-    <main className="flex flex-col items-center bg-slate-200">
+    <main className="flex flex-col items-center">
       <section className="min-h-screen bg-white w-[100vw] max-w-[440px] mx-auto overflow-y-auto overflow-x-hidden scrollbar-hidden">
         <Navbar />
         <Outlet />
