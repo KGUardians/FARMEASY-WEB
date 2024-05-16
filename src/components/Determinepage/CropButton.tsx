dc44ecc4cf8ae317bb5457da9944c34154a4aab0
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-05-16 22:36:38 +0900

    refactor: CropButton

--- src/components/Determinepage/CropButton.tsx
+++ src/components/Determinepage/CropButton.tsx
@@ -10,28 +10,27 @@ interface CropButtonProps {
 }
 
 export const CropButton = ({ item }: CropButtonProps) => {
-  const selectedCrop = useCropStore((state) => state.selectedCrop); 
+  const selectedCrop = useCropStore((state) => state.selectedCrop);
   const setSelectedCrop = useCropStore((state) => state.setSelectedCrop);
 
   const onItemClick = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     // 작물 취소 불가능
-    // setSelectedCrop(item.id); 
-    setSelectedCrop(selectedCrop === item.id ? null : item.id); 
-
+    // setSelectedCrop(item.id);
+    setSelectedCrop(selectedCrop === item.id ? null : item.id);
   };
 
   return (
     <div key={item.id} className="grid mb-8 place-items-center">
       <button
         onClick={onItemClick}
-        className={`flex rounded-[15px] items-center justify-center w-[52px] h-[52px] bg-neutral-100 hover:outline hover:outline-fp-green ${
-          selectedCrop === item.id ? 'outline outline-2 outline-fp-green' : ''
+        className={`flex rounded-[15px] items-center justify-center w-[52px] h-[52px] bg-neutral-100 hover:outline hover:outline-gray-200 ${
+          selectedCrop === item.id && 'outline outline-3 outline-fp-green'
         }`}
       >
         <img src={item.icons} alt={item.title} />
       </button>
-      <p className="my-1 text-xs text-center">{item.title}</p>
+      <p className="my-2 text-xs text-center">{item.title}</p>
     </div>
   );
 };
