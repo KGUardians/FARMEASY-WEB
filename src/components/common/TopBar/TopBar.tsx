0e8b398a0e90ffb7bb9435800082b96e649adee1
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-05-17 09:53:09 +0900

    refactor: TopBar

--- src/components/common/TopBar/TopBar.tsx
+++ src/components/common/TopBar/TopBar.tsx
@@ -3,16 +3,19 @@ import { IoChevronBackOutline } from 'react-icons/io5';
 
 interface TopBarProps {
   title: string;
+  right?: React.ReactNode;
 }
 
-const TopBar = ({ title }: TopBarProps) => {
+const TopBar = ({ title, right }: TopBarProps) => {
   const navigate = useNavigate();
   return (
-    <header className="flex items-center w-full h-16 px-3">
+    <header className="flex items-center justify-between w-full h-16 px-3">
       <button onClick={() => navigate(-1)} className="flex items-center gap-1">
         <IoChevronBackOutline />
         <span className="font-semibold">{title}</span>
       </button>
+
+      {right}
     </header>
   );
 };
