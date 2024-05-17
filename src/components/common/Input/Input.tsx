281cf526c914e608da9c300b753ed365268fdbff
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-05-18 00:01:13 +0900

    feat: Components (Input, ErrorMessage)

---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface InputProps {
+  label?: string;
+  placeholder?: string;
+  name?: string;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ label, placeholder, name, ...rest }, ref) => {
+    return (
+      <div>
+        {label && <p className="my-1 text-sm">{label}</p>}
+        <input
+          ref={ref} // ref를 input에 연결합니다.
+          placeholder={placeholder}
+          name={name}
+          {...rest} // 나머지 props를 input에 전달합니다.
+          className="w-full h-10 p-2 text-sm bg-gray-200 rounded-md outline-none"
+        />
+      </div>
+    );
+  },
+);
+
+export default Input;
