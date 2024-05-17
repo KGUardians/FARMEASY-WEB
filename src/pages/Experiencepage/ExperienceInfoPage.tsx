4a8277bf0e02becc2201268d7cc7c5b3fa135046
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-05-17 22:23:15 +0900

    feat: BottomBar

--- src/pages/Experiencepage/ExperienceInfoPage.tsx
+++ src/pages/Experiencepage/ExperienceInfoPage.tsx
@@ -1,3 +1,4 @@
+import BottomBar from '@components/ExperienceInfopage/BottomBar';
 import ExperienceDetailSection from '@components/ExperienceInfopage/ExperienceDetailSection';
 import { CarouselDefault } from '@components/common/Carousel/Carousel';
 import TopBar from '@components/common/TopBar/TopBar';
@@ -6,12 +7,14 @@ import { experienceInfoData } from '@mocks/mockData';
 const ExperienceInfoPage = () => {
   const data = experienceInfoData;
   return (
-    <section>
+    <section className="flex flex-col">
       <TopBar title="농촌체험" />
 
       <CarouselDefault url={data.url} />
 
       <ExperienceDetailSection data={data} />
+
+      <BottomBar />
     </section>
   );
 };
