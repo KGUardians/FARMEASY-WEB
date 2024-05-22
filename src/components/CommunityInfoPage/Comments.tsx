37d748e0094907eef8daddf80b83ce6c98f9e62a
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-05-22 18:42:42 +0900

    feat: handleSortClick

--- src/components/CommunityInfoPage/Comments.tsx
+++ src/components/CommunityInfoPage/Comments.tsx
@@ -1,11 +1,20 @@
 import { userInfo } from '@type/user';
+import { useState } from 'react';
 import kebab from '/kebab.svg';
+import { GoThumbsup } from "react-icons/go";
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 
 interface userInfoProps {
   data: userInfo;
 }
 
 const Comments = ({ data }: userInfoProps) => {
+  const [commentsSort, setCommentsSort] = useState<string>('최신순');
+
+  const handleSortClick = (sort: string) => {
+    setCommentsSort(sort);
+  };
+
   return (
     <div>
       <div className="flex justify-between my-4">
@@ -15,8 +24,18 @@ const Comments = ({ data }: userInfoProps) => {
         </div>
 
         <div className="flex gap-2 text-sm items-center">
-          <p>공감순</p>
-          <p>최신순</p>
+          <button
+            onClick={() => handleSortClick('최신순')}
+            className={commentsSort === '최신순' ? 'font-bold' : ''}
+          >
+            최신순
+          </button>
+          <button
+            onClick={() => handleSortClick('공감순')}
+            className={commentsSort === '공감순' ? 'font-bold' : ''}
+          >
+            공감순
+          </button>
         </div>
       </div>
 
@@ -37,16 +56,21 @@ const Comments = ({ data }: userInfoProps) => {
             <p className="my-1">저도 같은 문제로 고민중이에요 ㅠㅠ</p>
 
             <div className="flex gap-4 font-light text-sm">
-              <div className="flex gap-1">
+              <div className="flex gap-1 cursor-pointer">
+                <GoThumbsup className='w-5 h-5'/>
                 <p>좋아요</p>
                 <p>1</p>
               </div>
 
-              <p>답글쓰기</p>
+              <div className="flex gap-1 cursor-pointer">
+                <IoChatbubbleEllipsesOutline className='w-5 h-5 transform scale-x-[-1]'/>
+                <p>답글쓰기</p>
+              </div>
+
             </div>
           </div>
         </div>
-        <img className="ml-auto" src={kebab} alt="kebab" />
+        <img className="ml-auto cursor-pointer" src={kebab} alt={kebab} />
       </div>
     </div>
   );
