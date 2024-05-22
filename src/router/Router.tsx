55ae150533b5f093ce0284a822685b0adfb9d494
MinSeok Kim <kimminsuk051@gmail.com>
2024-05-22 10:35:54 +0900

    design: SignUpPage 퍼블리싱

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -8,6 +8,7 @@ import MarketPage from '@pages/Marketpage/MarketPage';
 import ExperienceInfoPage from '@pages/Experiencepage/ExperienceInfoPage';
 import ProductPage from '@pages/Marketpage/ProductPage';
 import ExperienceApplyPage from '@pages/Experiencepage/ExperienceApplyPage';
+import SignUpPage from '@pages/Signuppage/SignUpPage';
 
 const Router = () => (
   <RouterProvider
@@ -23,6 +24,7 @@ const Router = () => (
           { path: '/determine/result', element: <ResultPage /> },
           { path: '/market', element: <MarketPage /> },
           { path: '/market/:id', element: <ProductPage /> },
+          { path: '/signup', element: <SignUpPage /> },
         ],
       },
     ])}
