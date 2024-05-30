fe6912c41bb973eff8b8b24a016896f16398b7cf
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-05-30 23:00:43 +0900

    refactor: TitleBar

--- src/components/common/TitleBar/TitleBar.tsx
+++ src/components/common/TitleBar/TitleBar.tsx
@@ -10,11 +10,12 @@ export const TitleBar = ({ title, to }: TitleBarProps) => {
   return (
     <div className="flex items-center justify-between">
       <p className="font-semibold">{title}</p>
-      <Link to={to}></Link>
-      <div className="flex items-center gap-1">
-        <p className="text-sm font-light">더보기</p>
-        <IoChevronForwardOutline />
-      </div>
+      <Link to={to}>
+        <div className="flex items-center gap-1">
+          <p className="text-sm font-light">더보기</p>
+          <IoChevronForwardOutline />
+        </div>
+      </Link>
     </div>
   );
 };
