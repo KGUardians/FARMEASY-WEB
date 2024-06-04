4c451e84d9af00482122114191ce41d9a5814504
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-06-04 21:18:10 +0900

    feat: MarketPage
    - 백엔드 연동 (추후 작물별 분류기능 추가)

--- src/pages/MarketPage/MarketPage.tsx
+++ src/pages/MarketPage/MarketPage.tsx
@@ -1,17 +1,31 @@
+import { getMarketPost } from '@/apis/marketApi';
 import ProductListSection from '@components/MarketPage/ProductListSection';
 import ProductSelectCard from '@components/MarketPage/ProductSelectCard';
 import { Navbar } from '@components/common/Navbar/Navbar';
-import { marketData } from '@mocks/mockData';
+import { useQuery } from '@tanstack/react-query';
 
 const MarketPage = () => {
+
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['marketData'],
+    queryFn: getMarketPost,
+  });
+
+  if (isLoading) {
+    return <div>로딩 중</div>;
+  }
+
+  if (isError) {
+    console.error(error);
+    return <div>오류 발생</div>;
+  }
+
   return (
     <section className="grid pb-4">
       <Navbar />
-
       <div className="px-5 mt-3">
         <ProductSelectCard />
-
-        <ProductListSection productList={marketData} />
+        <ProductListSection productList={data} />
       </div>
     </section>
   );
